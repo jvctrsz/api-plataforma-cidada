71a52aee1e4ba33f76a1e4f7dd39ce5f41b7e2a7
jvctrsz <joaostem26@gmail.com>
2025-09-25 23:40:16 -0300

    remove dist

--- dist/Routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authControllers_1 = require("../Controller/authControllers");
-const authRouter = (0, express_1.Router)();
-authRouter.post("/auth/login", authControllers_1.authController.login);
-authRouter.post("/auth/register", authControllers_1.authController.register);
-authRouter.post("/auth/ativar-conta/:token", authControllers_1.authController.active);
-exports.default = authRouter;
--- dist/Utils/Errors/genericError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genericError = void 0;
-const genericError = (error, res) => {
-    console.error(error);
-    return res.status(500).json({ message: "Internal Server error!", error });
-};
-exports.genericError = genericError;
