81a7acb310110ff95b8ad015cf0b6bae87fc3503
jvctrsz <joaostem26@gmail.com>
2025-10-25 16:37:37 -0300

    coloca validação em get

--- src/Services/Request/messagesRequest.ts
+++ src/Services/Request/messagesRequest.ts
@@ -1,6 +1,7 @@
 import { UserRole } from "../../Controller/types";
 import { NotFoundError } from "../../Utils/Errors/CError";
 import { prisma } from "../../Utils/prisma";
+import { canHandleMessage } from "./Utils/canHandleMessage";
 
 export const messagesRequests = async (
   id: number,
@@ -10,6 +11,15 @@ export const messagesRequests = async (
   try {
     const request = await prisma.solicitacao.findUnique({ where: { id } });
     if (!request) throw new NotFoundError("Solicitação não encontrada.");
+    const user = await prisma.usuarios.findUnique({ where: { id: user_id } });
+    if (!user) throw new NotFoundError("Usuário não encontrado.");
+
+    canHandleMessage(
+      request.funcionario_id,
+      request.usuarios_id,
+      user_id,
+      role
+    );
 
     const messages = prisma.mensagens.findMany({
       where: { solicitacao_id: id },
